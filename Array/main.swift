d2fa5edf9bb426535674f8710859d72563cffe79
macleo <52macleo@gmail.com>
2018-05-26 01:12:50 +0800

    file and directory change
    file and directory change

---
+++ Array/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftStudy
+//
+//  Created by macleo on 25/05/2018.
+//  Copyright © 2018 macleo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Optional/main1.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SwiftStudy
+//
+//  Created by macleo on 25/05/2018.
+//  Copyright © 2018 macleo. All rights reserved.
+//
+
+import Foundation
+
+let person_name = "macleo"
+//print(person_name)
+
