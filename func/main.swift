5498efb0b480dd3f3699534f5ec1cf7fc3e684a5
macleo <52macleo@gmail.com>
2018-05-27 23:49:35 +0800

    define func
    define func

--- func/main.swift
+++ func/main.swift
@@ -13,9 +13,20 @@ let possibleNumber = "123"
 let convertedNumber = Int(possibleNumber)
 
 print(convertedNumber!)
-//Dictionary
-/*
-var dict:Dictionary = ["name":"macleo","age":40]
-print(dict["name"]! )
- */
 
+
+//define a function in swift 4.0
+func add(a :Int, b :Int) -> Int {
+    return a+b
+}
+
+print(add(a:5,b:6))
+
+func calcute(a:Double ,b:Double , method:(Double,Double)->Double)->Double{
+    return method(a,b)
+}
+func add (x:Double ,y:Double)->Double{
+    return x+y
+}
+
+print(calcute(a:5,b:6,method:add))
